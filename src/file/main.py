a88704da969de2a5383d183b64cfd6cb6ce72a03
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2023-02-06 18:44:45 +0900

    add file server

--- src/file/main.py
+++ src/file/main.py
@@ -1,34 +1,63 @@
-import uvicorn
 from fastapi import FastAPI, File, UploadFile
+from glob import glob
 
+import hashlib
+import uvicorn
 import shutil
 import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "local")
+if not os.path.exists(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+
+# def _hash_md5(file_bytes):
+#     """ md5 hash the file bytes"""
+#     md5 = hashlib.md5()
+#     md5.update(file_bytes[:])
+#     return md5.hexdigest()
+
 
 app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    return {"message": "Hello file"}
+
+
 @app.post("/files/")
-async def file(file: bytes = File(...)):
-    content = file.decode('utf-8')
-    formatfile = content.split('\n')
-    return {'filedetail': formatfile}
-
-
-@app.post("/uploadfile/")
-async def upload_file(file: UploadFile = File(...)):
-    return {'filename': file.filename}
-
-
-@app.post("/uploadfile/")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(file: UploadFile | None = None):
     if file:
+        # file_bytes = await file.read()
+        # file_md5 = _hash_md5(file_bytes)
         filename = file.filename
         fileobj = file.file
-        upload_dir = open(os.path.join(UPLOAD_DIR, filename), 'wb+')
-        shutil.copyfileobj(fileobj, upload_dir)
-        upload_dir.close()
-        return {"アップロードファイル名": filename}
-    return {"Error": "アップロードファイルが見つかりません。"}
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        with open(file_path, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+        return {"filename": filename, "message": "file uploaded successfully"}
+    return {"message": "File not found"}
+
+
+@app.delete("/files/")
+async def delete_file(file: UploadFile | None = None):
+    if file:
+        filename = file.filename
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            return {"message": "File deleted successfully"}
+    return {"message": "File not found"}
+
+
+@app.get("/files")
+async def get_files():
+    abs_paths = glob(os.path.join(UPLOAD_FOLDER, '*'))
+    rel_paths = [os.path.relpath(p, UPLOAD_FOLDER) for p in abs_paths]
+    return rel_paths
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
